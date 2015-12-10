7895bbaca0335da5216930a6ac58e3dd3e6d1976
Ryan Bird <ryan01bird@gmail.com>
2015-12-09 19:23:20 -0700

    initial commit

---
+++ app/controllers/bikes_controller.rb
@@ -0,0 +1,63 @@
+class BikesController < ApplicationController
+	def index
+		@bikes = Bike.all 
+	end
+
+	def show
+		@bike = Bike.find(params[:id])
+	end
+
+	def new
+	end
+
+	def edit 
+		@bike = Bike.find(params[:id])
+	end
+
+	def create
+		@bike = Bike.new(bike_params)
+		@bike.save
+		redirect_to @bike
+	end
+
+	def update
+		@bike = Bike.find(params[:id])
+
+		# if 
+		@bike.update(bike_params)
+		redirect_to @bike
+		# else
+		# 	render 'edit'
+		# end
+	end
+
+	def destroy
+		@bike = Bike.find(params[:id])
+		@bike.destroy
+		redirect_to bikes_path
+	end
+
+	private
+	def bike_params
+		params.require(:bike).permit(:name, :image)
+	end
+
+end
+
+
+# def update
+# 	  @article = Article.find(params[:id])
+	 
+# 	  if @article.update(article_params)
+# 	    redirect_to @article
+# 	  else
+# 	    render 'edit'
+# 	  end
+# 	end
+
+# 	def destroy
+# 	  @article = Article.find(params[:id])
+# 	  @article.destroy
+	 
+# 	  redirect_to articles_path
+# 	end
